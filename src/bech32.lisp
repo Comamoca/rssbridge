eb9a5cdb3388f798c7f38df0a69076c1908f052f
Comamoca <comamoca.dev@gmail.com>
2025-01-12 05:13:41 +0900

    :sparkles: (/src) Translate comment

--- src/bech32.lisp
+++ src/bech32.lisp
@@ -11,18 +11,18 @@
   (let* ((len (length hrp))
          (upper-bits (make-array len :element-type 'fixnum))
          (lower-bits (make-array len :element-type 'fixnum)))
-    ;; 上位ビットと下位ビットを1回のループで計算
+    ;; Calculate upper and lower bits in one loop
     (loop for i from 0 below len
           for c = (char-code (aref hrp i))
           do (setf (aref upper-bits i) (ash c -5))
              (setf (aref lower-bits i) (logand c 31)))
-    ;; リストに変換して結合
+    ;; Convert to list and join
     (append (coerce upper-bits 'list)
             '(0)
             (coerce lower-bits 'list))))
 
 (defun bech32-polymod (values)
-  "Bech32のチェックサムを計算する内部関数。"
+  "Internal function to calculate Bech32 checksum."
   (let* ((generator #(#x3b6a57b2 #x26508e6d #x1ea119fa #x3d4233dd #x2a1462b3))
          (chk 1))
     (dolist (value values chk)
@@ -33,10 +33,9 @@
             (setf chk (logxor chk (aref generator i)))))))))
 
 (defun bech32-create-checksum (hrp data spec)
-  "HRPとデータからチェックサム値を計算する関数。
-   hrp: Human-Readable Part
-   data: データ部分（リスト）
-   spec: エンコーディングタイプ（BECH32MまたはBECH32）"
+  "Function to calculate checksum value from HRP and data.
+   hrp: Human-Readable Part data: Data part (list)
+   spec: Encoding type (BECH32M or BECH32)"
   (let* ((values (append (bech32-hrp-expand hrp) data))
          (const (if (eq spec 'bech32m) BECH32M-CONST 1))
          (polymod (logxor (bech32-polymod (append values '(0 0 0 0 0 0))) const)))
@@ -46,24 +45,18 @@
 (defun bech32-verify-checksum (hrp data)
   "Verify a checksum given HRP and converted data characters."
   (let* ((const (bech32-polymod (append (bech32-hrp-expand hrp) data))))
-    ;; (when (= const 1)
-    ;;   'bech32)
-    ;; (when (= const BECH32M-CONST)
-    ;;   'bech32m)
-
     (cond
       ((= const 1) 'bech32)
       ((= const BECH32M-CONST) 'bech32m))))
 
 (defun bech32-encode (hrp data spec)
   (let* ((combined (append data (bech32-create-checksum hrp data spec)))
-	(head (uiop:strcat hrp "1"))
-	(tail (coerce (loop for d in combined collect (elt CHARSET d)) 'string))) 
+	 (head (uiop:strcat hrp "1"))
+	 (tail (coerce (loop for d in combined collect (elt CHARSET d)) 'string))) 
     (uiop:strcat head tail)))
 
 (defun bech32-decode (bech)
   "Validate a Bech32/Bech32m string, and determine HRP and data."
-
   (defun nil-any (sequence)
     (some (lambda (val) (not (null val))) sequence))
 
@@ -72,15 +65,15 @@
 	 (null-values '(nil nil nil)))
 
     (cond
-      ((nil-any (map 'list
-		     (lambda (x) (or (< (char-code x) 33) (> (char-code x) 126))) (coerce bech 'list)))
-       null-values)
+      ((nil-any (map 'list (lambda (x)
+			     (or (< (char-code x) 33) (> (char-code x) 126)))
+		     (coerce bech 'list))) null-values)
 
       ((or (< pos 1) (> (+ pos 7) (length bech)) (> (length bech) 90))
        null-values)
 
       ((every #'identity (map 'list (lambda (x)
-					   (null (search (string x) CHARSET))) (coerce (subseq bech (+ pos 1)) 'list)))
+				      (null (search (string x) CHARSET))) (coerce (subseq bech (+ pos 1)) 'list)))
        null-values)
 
       (t (progn
@@ -91,97 +84,5 @@
 
 	     (let ((data (subseq data 0 (- (length data) 6))))
 	       (if (null spec)
-		 null-values
-		 (list hrp data spec))) 
-	     ))))))
-
-;; (bech32-decode "bc1pzry9lf7nxe")
-;; (bech32-decode "npub10hqkwugj7p027j250qr9gwcuqpkwxftj0rjjk8y6hlmryu8dwp8s2runf2")
-
-
-;; (bech32-encode "npub"
-;; 	       (coerce (rssbridge/src/utils:hex->bytes  "8e1268c5f64362848bc0531e895d7489921ba2667e33e862433a0123ed5ca5a6") 'list)
-;; 	       'bech32)
-
-;; (let ((data '(1 2 3 4 5 31 9 30 19 6 25) ))
-;;   (subseq data 0 (- (length data) 6))
-;;   )
-
-
-;; (let* ((bech (to-lower "bc1pzry9lf7nxe"))
-;;        (pos (search "1" bech))) 
-
-;;   (let* ((hrp (subseq bech 0 pos))
-;; 	 (data (map 'list (lambda (x)
-;; 			    (search (string x) CHARSET)) (subseq bech (+ pos 1))))
-;; 	 (spec (bech32-verify-checksum hrp data))) 
-
-;;     ;; data
-;;     ;; (bech32-verify-checksum hrp '(1 2 3 4 5 31 9 30 19 6 25))
-;;     hrp
-;;     ))
-
-;; (let ((bech "pzry9lf7nxe"))
-;;   (map 'list (lambda (x)
-;; 	       (search (string x) CHARSET)) bech))
-
-;; (map 'list (lambda (x)
-;; 	     ) "pzry9lf7nxe")
-
-	  ;; (when (or (< pos 1) (> (+ pos 7) (length bech)) (> (length bech) 90))
-	  ;;     '(nil nil nil))
-	  ;; (when (not (every #'identity (map 'list (lambda (x)
-	  ;; 					    (null (search (string x) CHARSET))) (coerce (subseq bech (+ pos 1)) 'list))))
-	  ;;   '(nil nil nil))
-
-  ;; (if (nil-any (map 'list
-  ;; 		    (lambda (x) (or (< (char-code x) 33) (> (char-code x) 126))) (coerce bech 'list)))
-  ;;     '(nil nil nil)
-  ;;     (progn
-  ;; 	(let* ((bech (to-lower bech))
-  ;; 	       (pos (rfind-str "1" bech)))
-  ;; 	  (when (or (< pos 1) (> (+ pos 7) (length bech)) (> (length bech) 90))
-  ;; 	      '(nil nil nil))
-  ;; 	  (when (not (every #'identity (map 'list (lambda (x)
-  ;; 						    (null (search (string x) CHARSET))) (coerce (subseq bech (+ pos 1)) 'list))))
-  ;; 	    '(nil nil nil))
-  ;; 	  ))
-  ;;     ))
-
-
-;; (every #'identity '(nil nil))
-
-;; (bech32-decode "npub1g8tj7zgure2etccavmp7sdj5xn75en4h4tkzezx48zcmf23dqens6exr02")
-
-;; ---------------------- ;;
-
-;; (map 'string #'char-upcase "hello")
-;; (map 'list (lambda (char) (char-upcase char)) (coerce "hello" 'list))
-
-;;   (funcall (lambda (x)
-;;     (or (< (char-code x) 33) (> (char-code x) 126))) (char "a" 0))
-
-;; (some (lambda (val) (not (null val))) '(nil nil nil))
-
-;; (not (null 1))
-
-;; (append (bech32-hrp-expand "bc") '(1 2 3 4 5))
-
-;; (bech32-polymod (append (bech32-hrp-expand "bc") '(1 2 3 4 5)))
-
-;; (bech32-verify-checksum "bc" '(1 2 3 4 5))
-
-;; (bech32-encode "npub" '(8 7 11 18 30 2 8 28 3 25 10 25 11 24 24 29 12 27 1 30 16 13 18 20 6 19 30 20 25 19 21 23 21 11 22 2 25 2 6 21 7 2 24 27 9 10 17 13 0 25 19 16) 'bech32)
-
-;; (bech32-create-checksum "bc" '(1 2 3 4 5) 'bech32m)
-;; (bech32-encode "bc" '(1 2 3 4 5) 'bech32) ; => "bc1pzry9lf7nxe"
-;; (coerce (loop for d in '(1 2 3) collect (elt CHARSET d)) 'string)
-
-
-;; (defun bech32-create-checksum (hrp data spec)
-;;   (let* ((values (append (bech32-hrp-expand hrp) data))
-;; 	 (const (if (eq spec 'bech32m) BECH32M-CONST 1))
-;; 	 (polymod (logxor (bech32-polymod (append values '(0 0 0 0 0 0))) const)))
-
-;;     (loop for i from 0 below 6
-;; 	  collect (logand (ash polymod (- (* 5 (- 5 i)))) 31))))
+		   null-values
+		   (list hrp data spec)))))))))
--- src/nostr.lisp
+++ src/nostr.lisp
@@ -4,29 +4,29 @@
 
 (in-package #:rssbridge/src/nostr)
 
-;; shashtはデフォルトで構造体のkeyを大文字でシリアライズするため、小文字に変換している。
-;; また、構造体のkeyにハイフンが使われているため、それをアンダーバーに変換する処理も行っている。
+;; By default, shasht serializes structure keys in uppercase letters, so they are converted to lowercase letters.
+;; Also, since a hyphen is used in the key of the structure, we also convert it to an underscore.
 (defparameter shasht:*symbol-name-function* (lambda (sym) 
 					      (cl-ppcre:regex-replace-all "-" (string-downcase (symbol-name sym)) "_")))
 
-;; Nostrイベントの構造体
-;; idとsigは後で計算する都合上デフォルト値としてnilが指定されている
+;; Nostr event structure
+;; id and sig are specified as nil as default values ​​for later calculation
 (defstruct event
-  (pubkey nil :type string)              ; 32バイトの小文字16進数公開鍵(Hex)
-  (created-at nil :type integer)         ; Unixタイムスタンプ（秒）
-  (kind nil :type integer)               ; 0から65535の整数
-  (tags nil :type vector)                ; タグのリスト（ネストされた文字列リスト）
-  (content nil :type string)             ; 任意の文字列
-  (id nil)                               ; 32バイトの小文字16進数SHA256ハッシュ
-  (sig nil))                             ; 64バイトの小文字16進数署名
+  (pubkey nil :type string)              ;; 32-byte lowercase hexadecimal public key (Hex)
+  (created-at nil :type integer)         ;; Unix timestamp (seconds)
+  (kind nil :type integer)               ;; integer from 0 to 65535
+  (tags nil :type vector)                ;; list of tags (nested string list)
+  (content nil :type string)             ;; any string
+  (id nil)                               ;; 32-byte lowercase hex SHA256 hash
+  (sig nil))                             ;; 64-byte lowercase hex signature
 
 (defun make-event-hash (pubkey created-at kind tags content)
-  "イベントのidを算出する"
+  "Calculate event id"
   (let* ((tags (if (= (length tags) 0)
 		   :empty-array
 		   tags))
 	 (tags-txt (let ((*print-pretty* nil))
-		 (shasht:write-json tags nil)))
+		     (shasht:write-json tags nil)))
 	 (sig-string (format nil "[0,\"~A\",~A,~A,~A,\"~A\"]"
 			     pubkey
 			     created-at
@@ -35,7 +35,7 @@
 			     content)))
     (rssbridge/src/utils:string-to-sha256 sig-string)))
 
-;; イベントをBIP340で署名/検証する関数
+;; Function to sign/verify events with BIP340
 (defun sign (seckey message)
   (let* ((message (hex->bytes message))
        (sec-key-bytes (hex->bytes seckey))
@@ -61,7 +61,7 @@
 					 (hex->bytes (event-id event))
 					 (hex->bytes (event-sig event))))
 
-;; イベントをJSONにシリアライズ/デシリアライズする関数
+;; Function to serialize/deserialize events to JSON
 (defun event-to-json (event)
   (shasht:write-json event nil))
 
@@ -82,77 +82,3 @@
 	      :tags tags
 	      :content content
 	      :sig sig)))
-
-;; ----------------------------------------  SCRATCH ZONE ---------------------------------------- 
-
-;; (let* ((event (make-event :kind 1
-;; 			  :content "hello"
-;; 			  :tags #()
-;; 			  :pubkey "8e1268c5f64362848bc0531e895d7489921ba2667e33e862433a0123ed5ca5a6"
-;; 			  :created-at (get-now-unix-time))) 
-;;        (seckey "4e8dd4a103bf238ff0f7adb7f911ff8c9d65e869982ad80594858f230dc2908b")) 
-;;   ;; (verify-event event)
-
-;;   (shasht:write-json (sign-event event seckey) nil))
-
-
-
-
-;; ;; (unintern 'event (package-name *package*))
-
-;; (defun get-event ()
-;;   ""
-;;   "{\"content\": \"ぽわ～\",
-;;   \"created_at\": 1734731158,
-;;   \"id\": \"eb53bf9c97610a51d81b0a0a4ca2b101de33fd95638b6505c6cf8069adf098bd\",
-;;   \"kind\": 1,
-;;   \"pubkey\": \"4bcc022810a7eff571f1a02024cf3a835c795af109b4ecab11e45062299141d7\",
-;;   \"sig\": \"7f083be748cc69f1562819698cfbdd2fdcad7d71faac0245e7f9e5324e6337a8b95e0ea82a8de3e5918597a0996428319385fbfb2812dfc2a439aa0b4be6535a\",
-;;   \"tags\": []}")
-
-;; (let* ((message (rssbridge/src/utils:hex->bytes "01f9640fa49905074e5bdbb1e98d94ab521b00f4b2bd7558295f9f436dd26ad5"))
-;;        (pub-key-bytes (rssbridge/src/utils:hex->bytes "8e1268c5f64362848bc0531e895d7489921ba2667e33e862433a0123ed5ca5a6"))
-;;        (sec-key-bytes (rssbridge/src/utils:hex->bytes "4e8dd4a103bf238ff0f7adb7f911ff8c9d65e869982ad80594858f230dc2908b"))
-;;        (pub-key (make-instance 'ironclad:secp256k1-public-key :y pub-key-bytes))
-;;        (sec-key (make-instance 'ironclad:secp256k1-private-key :x sec-key-bytes))
-;;        ;; (sign (ironclad:sign-message sec-key message))
-;;        (sign (sign-message sec-key-bytes message))
-;;        )
-;;   (rssbridge/src/utils:bytes->hex sign)
-;;   )
-
-;; (let* ((event (make-event :kind 1
-;; 			  :content "hello"
-;; 			  :tags #()
-;; 			  :pubkey "8e1268c5f64362848bc0531e895d7489921ba2667e33e862433a0123ed5ca5a6"
-;; 			  :created-at (get-now-unix-time)))
-;;        (hash (make-event-hash (event-pubkey event)
-;; 			      (event-created-at event)
-;; 			      (event-kind event)
-;; 			      (event-tags event)
-;; 			      (event-content event)))
-;;        (sig (sign "4e8dd4a103bf238ff0f7adb7f911ff8c9d65e869982ad80594858f230dc2908b" hash)))
-
-;;     (setf (event-id event) hash)
-;;     (setf (event-sig event) sig)
-
-;;     )
-
-;; ;; (asdf:load-system :alexandria)
-;; ;; (asdf:load-system :ironclad)
-
-;; ;; pubkey: 8e1268c5f64362848bc0531e895d7489921ba2667e33e862433a0123ed5ca5a6
-;; ;; seckey: 4e8dd4a103bf238ff0f7adb7f911ff8c9d65e869982ad80594858f230dc2908b
-;; ;; id:     e467a852300352c1d8f42c7b678ce1106e18c5fa8fe5f6c3d62976950b83a288
-;; ;; sig:    a20edcc50f6f40e6ef7e2a01ea328e1a18a5d6076e57962370154915ec6c4c2c1cf84de0f2e3f0c5c8fcd4a46ec7bd0dc955519da9ee71580a59e4053d77b0b8
-
-
-;; (progn 
-;;   (asdf:load-system :rssbridge/src/bip340)
-;;   (asdf:load-system :rssbridge/src/utils)
-;;   (asdf:load-system :shasht)
-;;   (asdf:load-system :local-time)
-;;   (asdf:load-system :babel)
-;;   (asdf:load-system :ironclad)
-;;   (asdf:load-system :cl-ppcre))
-
